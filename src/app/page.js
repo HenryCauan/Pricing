549d4cfef4b815a50830218edbf5b30cb0d7325b
HenryCa <henrycauan3222@gmail.com>
2025-04-29 17:30:18 -0300

    :finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,113 @@
 import Image from "next/image";
+import 'remixicon/fonts/remixicon.css'
+import Stars from "../assets/bg-pattern.png"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="min-w-screen h-screen flex flex-col  text-[color:#121826] select-none">
+        <div className="flex flex-col justify-center items-center w-full h-[20%] bg-[color:#f9fafd] text-[color:#121826] border-b-1 border-[color:#E5E7EB] relative">
+          <Image
+            src={Stars} // Substitua pelo caminho da sua imagem
+            alt="Background"
+            fill
+            className="object-cover z-[1]"
+          />
+          <h2 className="text-4xl mb-4 font-bold relative z-10">Pricing & Plans</h2>
+          <p className="text-center whitespace-nowrap relative z-10 max-sm:whitespace-normal">Discover the plan that unlocks the transformative power <br/> of digital art and join our vibrant community today</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="text-black p-6 items-center" id="grid">
+          <div className="flex flex-col w-full" id="one">
+            <div className="flex flex-col gap-2 w-full h-[8.5rem] justify-center items-center mb-2">
+              <span className="text-[color:#263FA9] font-semibold hidden">Free</span>
+              <span className="text-6xl font-bold hidden">$0</span>
+              <span className="hidden">Per month</span>
+            </div>
+            <div className="flex flex-col bg-[color:#f9fafc] rounded-tl-lg rounded-bl-lg whitespace-nowrap">
+              <span className="py-2 px-4 font-semibold border-b-1 border-[color:#E5E7EB]">Wesbite number</span>
+              <span className="py-2 px-4 font-semibold border-b-1 border-[color:#E5E7EB]">Server storage</span>
+              <span className="py-2 px-4 font-semibold border-b-1 border-[color:#E5E7EB]">Database</span>
+              <span className="py-2 px-4 font-semibold border-b-1 border-[color:#E5E7EB]">Unmetered width</span>
+              <span className="py-2 px-4 font-semibold border-b-1 border-[color:#E5E7EB]">SSD Disk</span>
+              <span className="py-2 px-4 font-semibold border-b-1 border-[color:#E5E7EB]">VCPUS Fontworld</span>
+              <span className="py-2 px-4 font-semibold border-b-1 border-[color:#E5E7EB]">Worldpress install</span>
+              <span className="py-2 px-4 font-semibold border-b-1 border-[color:#E5E7EB]">Server speed</span>
+            </div>
+          </div>
+          <div className="flex flex-col" id="two">
+            <div className="flex flex-col gap-2 w-full h-[11rem] justify-center items-center mb-2">
+              <span className="text-[color:#263FA9] font-semibold">Free</span>
+              <span className="text-6xl font-bold ">$0</span>
+              <span className="opacity-65">Per month</span>
+            </div>
+            <div className="flex flex-col items-center w-full mb-4">
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium">01</span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium">100 GB</span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+            </div>
+            <div className="flex gap-2 justify-center w-full font-bold text-[color:#263FA9] ">Get Started <i className="ri-arrow-right-line"></i></div>
+          </div>
+          <div className="flex flex-col" id="three">
+            <div className="flex flex-col gap-2 w-full h-[11rem] justify-center items-center mb-2">
+              <span className="text-[color:#263FA9] font-semibold">Team</span>
+              <span className="text-6xl font-bold">$59</span>
+              <span className="opacity-65">Per month</span>
+            </div>
+            <div className="flex flex-col items-center w-full mb-4">
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium">10</span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium">500 GB</span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> 15 </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> ✓ </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> - </span>
+            </div>
+            <div className="flex gap-2 justify-center w-full font-bold text-[color:#263FA9] ">Get Started <i className="ri-arrow-right-line"></i></div>
+          </div>
+          <div className="flex flex-col bg-[color:#121826] text-white rounded-2xl mt-5" id="four">
+            <div className="flex flex-col gap-2 w-full h-[11rem] justify-center items-center mb-0">
+              <span className="text-white bg-[color:#263FA9] rounded-3xl px-3 font-semibold">Popular</span>
+              <span className="text-6xl font-bold">$99</span>
+              <span className="opacity-65">Per month</span>
+            </div>
+            <div className="flex flex-col items-center w-full">
+              <span className="py-2 border-b-1 w-full text-center border-[color:#101622] font-medium">50</span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#101622] font-medium">1 TB</span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#101622] font-medium"> Unlimited </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#101622] font-medium"> ✓ </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#101622] font-medium"> ✓ </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#101622] font-medium"> ✓ </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#101622] font-medium"> ✓ </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#101622] font-medium"> ✓ </span>
+            </div>
+            <div className="flex gap-2 justify-center items-center w-full h-13 font-bold text-white bg-[color:#7d4f1e] rounded-bl-2xl rounded-br-2xl">Get Started <i className="ri-arrow-right-line"></i></div>
+          </div>
+          <div className="flex flex-col" id="five">
+            <div className="flex flex-col gap-2 w-full h-[11rem] justify-center items-center mb-2">
+              <span className="text-[color:#263FA9] font-semibold">Enterprise</span>
+              <span className="text-6xl font-bold">$299</span>
+              <span className="opacity-65">Per month</span>
+            </div>
+            <div className="flex flex-col items-center w-full mb-4">
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium">Unlimited</span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium">Unlimited</span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium">Unlimited</span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> ✓ </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> ✓ </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> ✓ </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> ✓ </span>
+              <span className="py-2 border-b-1 w-full text-center border-[color:#E5E7EB] font-medium"> ✓ </span>
+            </div>
+            <div className="flex gap-2 justify-center w-full font-bold text-[color:#263FA9] ">Get Started <i className="ri-arrow-right-line"></i></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"vietnam"', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} 
